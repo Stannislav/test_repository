f1cc5998ba111751603432e709b9ff16772cace4
Stanislav Schmidt <stanislav.schmidt@outlook.com>
2019-06-25 00:16:30 +0200

    Removed unnecessary CI tests, added info to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
-NAME = 'my_package'
+
+NAME = 'my-package'
+VERSION = '1.0'
 DESCRIPTION = 'My test package with absolutely no content'
-VERSION = '1.0.0'
+AUTHOR = 'Stanislav Schmidt'
+PACKAGES = ['my_package']
+URL = 'https://github.com/Stannislav/test_repository'
 
 config = {
         'name': NAME,
-        'description': DESCRIPTION,
         'version': VERSION,
+        'description': DESCRIPTION,
+        'author': AUTHOR,
+        'packages': PACKAGES,
+        'url': URL,
 }
 
 setup(**config)
